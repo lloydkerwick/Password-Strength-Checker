c3216c3a9820b6e06976559882879aa1b6bedc13
lloydkerwick <lloyd.kerwick@gmail.com>
2025-01-24 04:38:01 +1000

    first commit

---
+++ scripts.js
@@ -0,0 +1,61 @@
+const passwordInput = document.querySelector('.js-password-input');
+const setPasswordBtn = document.querySelector('.js-set-password');
+const passwordStrength = document.querySelector('.js-password-strength');
+
+passwordInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    setPasswordBtn.focus();
+  }
+});
+
+function isValidPassword(password) {
+  const regex = /^[A-Za-z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{6,16}$/;
+  return regex.test(password);
+}
+
+function GetpasswordStrength(password) {
+  let strength = 0;
+  if (password.length > 8) strength++;
+  if (/[A-Z]/.test(password)) strength++;
+  if (/[a-z]/.test(password)) strength++;
+  if (/[0-9]/.test(password)) strength++;
+  if (/[^A-Za-z0-9]/.test(password)) strength++; 
+  console.log('Password Strength:', strength); 
+  return strength;
+}
+
+setPasswordBtn.addEventListener('click', () => {
+  const password = passwordInput.value;
+  console.log('Password entered:', password); 
+
+  if (!isValidPassword(password)) {
+    passwordStrength.textContent = 'Please enter a valid password';
+    passwordStrength.style.color = 'red';
+    return;
+  }
+
+  const strength = GetpasswordStrength(password); 
+  let strengthText = '';
+  switch (strength) {
+    case 5: 
+      strengthText = 'Very Strong';
+      passwordStrength.style.color = 'green';
+      break;
+    
+    case 4:
+      strengthText = 'Strong';
+      passwordStrength.style.color = 'limegreen';
+      break;
+
+    case 3:
+      strengthText = 'Moderate';
+      passwordStrength.style.color = 'orange';
+      break;
+
+    default:
+      strengthText = 'Weak';
+      passwordStrength.style.color = 'red';
+  }
+
+  passwordStrength.textContent = `Your password is valid. Password strength: ${strengthText}`;
+});
